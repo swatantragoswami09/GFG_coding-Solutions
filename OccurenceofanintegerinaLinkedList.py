5b01644ddaee4729983614dd6f57ec84441e9598
Swatantra <54662673+swatantragoswami09@users.noreply.github.com>
2020-05-18 23:01:57 +0530

    Update OccurenceofanintegerinaLinkedList.py

--- OccurenceofanintegerinaLinkedList.py
+++ OccurenceofanintegerinaLinkedList.py
@@ -1,1 +1,71 @@
-Occurence of an integer in a Linked List
+"""  Return the no. of times search_for value is there in a linked list.
+  The input list will have at least one element
+  Node is defined as
+
+class Node:
+    def __init__(self, data):
+		self.data = data
+		self.next = None
+  This is method only submission.
+  You only need to complete the method.
+"""
+from collections import Counter
+def count(head, search_for):
+    p=head
+    l=[]
+    found=0
+    while p:
+        l.append(p.data)
+        p=p.next
+    d=Counter(l)
+    for k,v in d.items():
+        if k==search_for:
+            found=v
+            break
+    return found
+        
+    
+
+
+
+#{ 
+#  Driver Code Starts
+# Do not delete this comment
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def push(self, new_data):
+        new_node = Node(new_data)
+        new_node.next = self.head
+        self.head = new_node
+
+    def printList(self):
+        temp = self.head
+        while (temp):
+            print(temp.data, end=" ")
+            # arr.append(str(temp.data))
+            temp = temp.next
+        print()
+
+
+if __name__ == '__main__':
+    t = int(input())
+    while (t > 0):
+        llist = LinkedList()
+        n = int(input())
+        values = list(map(int, input().split()))
+        for i in reversed(values):
+            llist.push(i)
+        m = int(input())
+        k = count(llist.head, m)
+        print(k)
+        t -= 1
+# Contributed by:Harshit Sidhwa
+# } Driver Code Ends
