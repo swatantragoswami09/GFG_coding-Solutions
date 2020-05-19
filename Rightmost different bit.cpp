08d9f0e7254cfb2c43a5202d32e0fb4009ba282b
Swatantra <54662673+swatantragoswami09@users.noreply.github.com>
2020-05-19 21:19:50 +0530

    Create Rightmost different bit.cpp

---
+++ Rightmost different bit.cpp
@@ -0,0 +1,42 @@
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+// Function to get rightmost set bit
+int getRightMostSetBit(int n)
+{
+    return log2(n & -n) + 1; //finding the rightmost set bit of a number
+}
+ 
+
+ // } Driver Code Ends
+//User function Template for C++
+
+/*  Function to find the first position with different bits
+*   This function returns the position with different bit
+*/
+int posOfRightMostDiffBit(int m, int n)
+{
+    
+    n=m^n;
+    return log2(n&-n)+1;
+    
+    
+}
+
+// { Driver Code Starts.
+
+// Driver Code
+int main()
+{   
+    int t;
+    cin>>t; //input number of testcases
+    while(t--)
+    {
+         int m,n;
+         cin>>m>>n; //input m and n
+         cout << posOfRightMostDiffBit(m, n)<<endl;
+    }
+    return 0;     
+}   // } Driver Code Ends
