973eb70af72889441cd1466505e9c61acd801443
Swatantra <54662673+swatantragoswami09@users.noreply.github.com>
2020-05-25 13:19:44 +0530

    Create Left Most Digit of a product.cpp

---
+++ Left Most Digit of a product.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+		int t;
+	cin>>t;
+	while(t-->0)
+	{
+	    int n;
+	    double s=0;
+	    cin>>n;
+	    long long a[n];
+	    for(int i=0;i<n;i++)
+	    {
+	        cin>>a[i];
+	        s+=log10(a[i]);
+	    }
+	  
+	    cout<<(int)(pow(10,s-(int)(s)))<<"\n";
+	}
+	return 0;
+}
