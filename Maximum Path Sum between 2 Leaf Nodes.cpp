32cfff6ceef63c495784fe090264c74a95783f18
Swatantra <54662673+swatantragoswami09@users.noreply.github.com>
2021-06-25 15:36:16 +0530

    Update Maximum Path Sum between 2 Leaf Nodes.cpp

--- Maximum Path Sum between 2 Leaf Nodes.cpp
+++ Maximum Path Sum between 2 Leaf Nodes.cpp
@@ -90,24 +90,33 @@ struct Node
     }
 };
 */
-
 class Solution {
-public:
-    int path(Node *root,int &ref){
-        if(root==NULL)
+    int maxPathSumUtil(Node *root, int &res) {
+
+
+        if (root == NULL)
             return 0;
-       int l=path(root->left,ref);
-       int r=path(root->right,ref);
-       int loc=max(max(l,r)+root->data,root->data);
-       int glob=max(loc,l+r+root->data);
-       ref=max(glob,ref);
-       return loc;
+        if (!root->left && !root->right)
+            return root->data;
+
+        int ls = maxPathSumUtil(root->left, res);
+        int rs = maxPathSumUtil(root->right, res);
+
+        if (root->left && root->right) {
+            res = max(res, ls + rs + root->data);
+            return max(ls, rs) + root->data;
+        }
+        return (!root->left) ? rs + root->data : ls + root->data;
     }
-    int maxPathSum(Node* root)
-    {
-        int ref;
-        path(root,ref);
-        return ref;
+public:
+    int maxPathSum(Node *root) {
+        int res = INT_MIN;
+        int h = maxPathSumUtil(root, res);
+        if (root->left == NULL || root->right == NULL)
+        {
+            res = max(res, h);
+        }
+        return res;
     }
 };
 
